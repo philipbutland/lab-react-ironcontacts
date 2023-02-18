865b7eba66c9a473416a14662d8410cc98aca16c
Philip Butland <Philip_Butland@yahoo.com>
2023-02-18 17:00:35 +0100

    Iteration 3

--- src/App.js
+++ src/App.js
@@ -1,20 +1,48 @@
-import './App.css';
-import Contacts from "./contacts.json"
-import Celebrity from './components/Celebrity';
+import "./App.css";
+import Contacts from "./contacts.json";
+import Celebrity from "./components/Celebrity";
+import { useState } from "react";
 
-
+const displayContacts = Contacts.slice(0, 5);
 
 function App() {
-  const fiveContacts = Contacts.slice(0,5)
+  const [newContacts, setNewContacts] = useState(displayContacts);
+
+  function AddContact() {
+    const random = Contacts[Math.floor(Math.random() * Contacts.length)];
+    // console.log("Math: ", Math.floor(Math.random() * Contacts.length), Contacts.length)
+    console.log(random);
+    console.log(displayContacts);
+    setNewContacts(prevContacts => {
+      const newList = [random, ...prevContacts]
+
+      return newList
+    });
+
+  }
+
   return (
     <div className="App">
-    <table>
-      <tr><th>Picture</th><th>Name</th><th>Popularity</th></tr>
-      {fiveContacts.map(contact => {
-        return <Celebrity contact={contact}></Celebrity>
-      })}
-    </table>
+      {/* <button>Add Random Contact</button> */}
+      <button
+        onClick={() => {
+          AddContact();
+        }}
+      >
+        Add Random Contact
+      </button>
 
+      <table>
+        <tr>
+          <th>Picture</th>
+          <th>Name</th>
+          <th>Popularity</th>
+        </tr>
+        {newContacts.map((contact) => {
+          return <Celebrity contact={contact}></Celebrity>;
+        })}
+        {/* {newContacts.name} */}
+      </table>
     </div>
   );
 }
